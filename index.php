4fcbda78b4ed09aaa86986f235d8d62a4f37592b
Quique <Quique@ThePirateCat>
2014-05-08 00:40:04 +0100

    Cambios

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
+<!-- Opera is so weird and fun http://[::1]:8000/ -->
+
+<?php
+    function initDB() {
+        try {
+            $db = new PDO("sqlite:./data.base");
+            return($db);
+        } catch (PDOException $e) {
+            print '<p>Error: '.$e->getMessage().'</p>';
+        }
+    }
+
+    $db = initDB(); // ould not find driver....
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <title>Práctica 6 - PHP</title>
     </head>
     <body>
-        <h1>TEST</h1>
-        <?php
-            echo "Hello world from PHP"
-        ?>
+        <h1>Práctica 6</h1>   
+
+         <!-- <?php phpinfo(); ?> -->
     </body>
 </html>
