65bccc53a98ea2a05e14f7926d0c7fea1861f42e
Quique <Quique@ThePirateCat>
2014-05-07 23:15:33 +0100

    Hello world

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>TEST</h1>
+        <?php
+            echo "Hello world from PHP"
+        ?>
+    </body>
+</html>
