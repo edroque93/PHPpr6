ef49fd773daae61b8d7b2e01bf8307fe7db4bacc
Quique <Quique@nerv.(none)>
2014-05-13 14:11:36 +0000

    Stuff

--- database_create_fill.php
+++ database_create_fill.php
@@ -1,5 +1,3 @@
-<!-- Opera is so weird and fun http://[::1]:8000/ -->
-
 <?php
     function initDB() {
         try {
@@ -10,13 +8,10 @@
         }
     }
 
-    $db = initDB(); // ould not find driver....
+    $db = initDB();
     
-    $db->exec("CREATE TABLE IF NOT EXISTS messages (
-                    id INTEGER PRIMARY KEY, 
-                    title TEXT, 
-                    message TEXT, 
-                    time INTEGER)");
+    $db->exec(file_get_contents("create_database_structure.sql"));
+    $db->exec(file_get_contents("create_mock.sql"));
 ?>
 
 <!DOCTYPE html>
@@ -27,8 +22,6 @@
         <title>Práctica 6 - PHP</title>
     </head>
     <body>
-        <h1>Práctica 6</h1>   
-
-         <!-- <?php phpinfo(); ?> -->
+        <h1>Script de carga completado!</h1>   
     </body>
 </html>
