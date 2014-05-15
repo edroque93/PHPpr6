e936fda1c8c381b36bcea6c8dd6668d0b3c04ab9
edroque93 <quique.lpgc@gmail.com>
2014-05-15 19:42:32 +0000

    Update

--- login.php
+++ login.php
@@ -1,32 +1,17 @@
 <?php
-	function getDB() {
-		try {
-			$db = new PDO("sqlite:./data.base");
-			return($db);
-		} catch (PDOException $e) {
-			print "<p>Error: ".$e->getMessage()."</p>";
-		}
-	}
-	$db = getDB();
+	include_once "db_utils.php";
+	global $db;
 	
 	session_start();
 	
 	$user = $_POST['username'];
 	$pass = md5($_POST['password']);
 	
-	$query = "select * from usuarios";
+	$query = "select * from usuarios where identificador=\"$user\"";
 	
-	print $query;
-	
-
-
 	$result = $db->query($query);
 	
-	print $result->rowCount()."\n";
-	
-	if ($result->rowCount() == 1) {
-		$_SESSION['user'] = $result->fetch();
-		
+	if ($_SESSION['user'] = $result->fetch()) {
 		print $_SESSION['user']['id'];
 	} else {
 		print "nope.";
