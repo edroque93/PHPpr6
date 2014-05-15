0567c2e7f44fa59c71242bf19076bd6dc15092cb
edroque93 <quique.lpgc@gmail.com>
2014-05-15 19:37:14 +0000

    Error

--- login.php
+++ login.php
@@ -1,9 +1,36 @@
 <?php
+	function getDB() {
+		try {
+			$db = new PDO("sqlite:./data.base");
+			return($db);
+		} catch (PDOException $e) {
+			print "<p>Error: ".$e->getMessage()."</p>";
+		}
+	}
+	$db = getDB();
+	
 	session_start();
 	
-	// Check user exists in db and stuff...
+	$user = $_POST['username'];
+	$pass = md5($_POST['password']);
 	
-	$_SESSION['username'] = $_POST['username'];
-	$_SESSION['password'] = $_POST['password'];
-	header("Location: index.php");
+	$query = "select * from usuarios";
+	
+	print $query;
+	
+
+
+	$result = $db->query($query);
+	
+	print $result->rowCount()."\n";
+	
+	if ($result->rowCount() == 1) {
+		$_SESSION['user'] = $result->fetch();
+		
+		print $_SESSION['user']['id'];
+	} else {
+		print "nope.";
+	}
+
+	//header("Location: index.php");
 ?>
