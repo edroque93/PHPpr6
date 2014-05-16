8264f43087300fa329e20da9396bc699e76579d7
edroque93 <quique.lpgc@gmail.com>
2014-05-16 00:34:41 +0000

    Update

--- routes.php
+++ routes.php
@@ -6,7 +6,30 @@
 <?php
 	include "header.php";
 	
-	echo '<p>Hola de vuelta</p>';
+	include "db/db_utils.php";
 
+	$activities = getTable("actividades");
+	$userplans = getTable("inscripciones");
+	
+	echo "<table><tr>";
+
+/*
+	foreach($activities["columns"] as $column) {
+	  echo "<th>$column</th>";
+	}*/
+
+	echo "</tr>";
+
+	foreach($activities["data"] as $row) {
+  		echo '
+  			<div>
+  				<h1><a href="'.$row[url].'">'.$row[nombre].'</a></h1>
+  				<h4>'.substr($row[fecha], 0, -7).'</h4>
+  				<p>'.$row[descripcion].'</p>
+  			</div>';
+	}
+	
+	echo "</table>";
+	
 	include "footer.php";
 ?>
